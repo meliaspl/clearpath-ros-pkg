8c1158fc02f4d0e6114d048fe1bf185ab4ca2f3a
mpurvis <mpurvis@clearpathrobotics.com>
2011-11-24 19:23:23 +0000

    Remove old avr_bridge firmware.

--- clearpath_chameleon/chameleon_avr_bridge/arduino_cmake/cmake_scripts/arduino_uno.cmake
+++
@@ -1,8 +0,0 @@
-set(ARDUINO_PROTOCOL "stk500v1")
-set(ARDUINO_BOARD "atmega328p")
-set(ARDUINO_MCU "m328p")
-set(ARDUINO_FCPU "16000000")
-set(ARDUINO_UPLOAD_SPEED "115200")
-set(ARDUINO_PORT "/dev/ttyACM0")
-
-include("${chameleon_avr_bridge_PACKAGE_PATH}/arduino_cmake/cmake_scripts/arduino.cmake")
